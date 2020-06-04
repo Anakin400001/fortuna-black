8e4bed7f36fa1b5964a59e9335a6db4f04c69ba3
Anakin400001 <58471681+Anakin400001@users.noreply.github.com>
2020-06-04 10:32:32 +0300

    Updates

---
+++ precache-manifest.e1b035e3539e002cfce7dc5c5735567a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3eac873a40abfa7cb1dfe2d8000c93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d3166059b170139c335",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "bf4803f09921b8d600bf",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "7d3166059b170139c335",
+    "url": "/static/js/2.480d77b9.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.480d77b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf4803f09921b8d600bf",
+    "url": "/static/js/main.42941618.chunk.js"
+  },
+  {
+    "revision": "65cb32bc103b08dbb280",
+    "url": "/static/js/runtime-main.5a5050e3.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
