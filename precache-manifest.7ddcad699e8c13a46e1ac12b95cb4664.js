d740c705f7ba1f07ee01cf2067e7fb27171dd787
Anakin400001 <58471681+Anakin400001@users.noreply.github.com>
2020-06-06 16:42:34 +0300

    Updates

--- precache-manifest.7ddcad699e8c13a46e1ac12b95cb4664.js
+++ precache-manifest.7ddcad699e8c13a46e1ac12b95cb4664.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eff043f2c39f541bb5af6131a0b72372",
+    "revision": "98d6c70f7ae0773fee4049985e6cf6d2",
     "url": "./index.html"
   },
   {
-    "revision": "e0ae06e2abe7e859a8cc",
+    "revision": "6953bcc7249c933b71b9",
     "url": "./static/css/2.86df01db.chunk.css"
   },
   {
-    "revision": "3b060071a06e2b218413",
+    "revision": "3043f913493ee18f4542",
     "url": "./static/css/main.834ac9a1.chunk.css"
   },
   {
-    "revision": "e0ae06e2abe7e859a8cc",
-    "url": "./static/js/2.259dd3eb.chunk.js"
+    "revision": "6953bcc7249c933b71b9",
+    "url": "./static/js/2.8382c78d.chunk.js"
   },
   {
     "revision": "21ced859ea2b2d6b856d461ad6c2afed",
-    "url": "./static/js/2.259dd3eb.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.8382c78d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3b060071a06e2b218413",
-    "url": "./static/js/main.3726d400.chunk.js"
+    "revision": "3043f913493ee18f4542",
+    "url": "./static/js/main.c09424e0.chunk.js"
   },
   {
     "revision": "d935644ccdba11ed4514",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.214fbeffe7fc3da584f9f90d6268e889.js"
+  "./precache-manifest.7ddcad699e8c13a46e1ac12b95cb4664.js"
 );
 
 self.addEventListener('message', (event) => {
